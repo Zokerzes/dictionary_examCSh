3b2b4ad870952799ee10d39a67d4c9661b67c9ed
Bliznyuk Roman <romzes85@mail.ru>
2022-10-02 19:00:34 +0300

    Добавьте файлы проекта.

---
+++ dictionary_examCSh/Word.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dictionary_examCSh
+{
+    public class Word
+    {
+        public string Russian { get; set; }
+        public string English { get; set; }
+    }
+}
