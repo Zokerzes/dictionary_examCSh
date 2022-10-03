ebc900e605221fd22a149b3cff59163f05aac5af
Zokerzes <romzes85@mail.ru>
2022-10-03 18:21:03 +0300

    +

--- dictionary_examCSh/Program.cs
+++ dictionary_examCSh/Program.cs
@@ -1,16 +1,8 @@
-﻿using dictionary_examCSh;
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using static System.Console;
+﻿using static System.Console;
 
 namespace dictionary_examCSh
-{ 
-   
+{
+
     class Program
     {
         
